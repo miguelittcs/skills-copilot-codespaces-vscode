12948dc26e93db581975e186a7c5fbce6278ca32
Miguel Espinosa <miguelittcs@gmail.com>
2025-04-15 02:27:54 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,28 @@
+//Create a web server
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+app.use(cors());
+app.use(bodyParser.json());
+
+let comments = [];
+
+// Get all comments
+app.get('/comments', (req, res) => {
+  res.json(comments);
+});
+
+// Add a new comment
+app.post('/comments', (req, res) => {
+  const comment = req.body;
+  comments.push(comment);
+  res.status(201).json(comment);
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
